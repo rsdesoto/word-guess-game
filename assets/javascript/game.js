4aba877d25e9d462c796fcbcc1def4fe9d5bcc4b
Ry <ry.e.desoto@gmail.com>
2018-11-09 23:16:54 -0600

    finished basic logic - some clean-up needed for edge cases

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,6 +37,8 @@ var validKeys = [
 var wins;
 // number of games played
 var gamesPlayed;
+// number of games lost
+var gamesLost;
 
 ////////////// for a single round ////////////
 // number of guesses made this round
@@ -54,8 +56,41 @@ var wordDisplayString;
 // get the div we are displaying the word itself into
 var displayMe = document.getElementById("wordguess");
 
+var displayGuessed = document.getElementById("guessedletters");
+
+var winDisplay = document.getElementById("windisplay");
+
+var gameDisplay = document.getElementById("gamedisplay");
+
+var lossDisplay = document.getElementById("lossdisplay");
+
+var roundEndText = document.getElementById("roundendtext");
+
+var continueText = document.getElementById("continue");
+
+continueText.onclick = function() {
+  clearGuesses();
+  setWord();
+  writeWord();
+  showWord();
+  showGuessed();
+  writeStats();
+};
+
+function winContent() {
+  continueText.style.display = "block";
+  roundEndText.style.display = "block";
+  roundEndText.textContent = "You won! The answer was " + questionWord;
+}
+
+function loseContent() {
+  continueText.style.display = "block";
+  roundEndText.style.display = "block";
+  roundEndText.textContent = "You lost :c the answer was " + questionWord;
+}
+
 // get the total number of guesses you get
-var guessesAllowed = 12;
+var guessesAllowed = 10;
 
 // create a function to clear out guesses for every round of the game
 function clearGuesses() {
@@ -63,16 +98,28 @@ function clearGuesses() {
   guessedLetters = "";
   wordDisplay = [];
   wordDisplayString = "";
+  roundEndText.style.display = "none";
+  continueText.style.display = "none";
 }
 
 // create a function to clear out wins, games played, guesses to initialize/restart the game
 function initializeGame() {
   wins = 0;
   gamesPlayed = 0;
+  gamesLost = 0;
+
   clearGuesses();
 }
 
+// create a function to write wins, losses, and games played
+function writeStats() {
+  winDisplay.textContent = wins;
+  lossDisplay.textContent = gamesLost;
+  gameDisplay.textContent = gamesPlayed;
+}
+
 initializeGame();
+writeStats();
 
 // randomly choose a word from the word list
 function setWord() {
@@ -97,15 +144,21 @@ function showWord() {
   displayMe.textContent = wordDisplayString;
 }
 
+function showGuessed() {
+  displayGuessed.textContent = guessedLetters;
+}
+
 setWord();
 writeWord();
 showWord();
+showGuessed();
 
 console.log(guessesAllowed);
+
 // when a key is pressed, check to see if the letter is in the chosen word
 document.onkeyup = function(event) {
   // only keep going until we reach the total guesses allowed
-  if (guessesMade < 13 && wordDisplayString.indexOf("_") > -1) {
+  if (guessesMade < guessesAllowed && wordDisplayString.indexOf("_") > -1) {
     console.log("Button: " + event.key);
     // 0th level: make sure this is an ACTUAL LETTER, since guessing "meta" will mess everything up
     ltr = event.key.toLowerCase();
@@ -120,11 +173,15 @@ document.onkeyup = function(event) {
             }
           }
           showWord();
+        } else {
+          // if the letter is NOT in the guessing word, increment up the guessesMade number
+          guessesMade++;
         }
 
-        // increment guesses made counter up and add the new letter to guessed letters
-        guessesMade++;
+        // add the new letter to guessed letters and update the showGuessed text section
         guessedLetters += ltr;
+
+        showGuessed();
 
         // temp test - print all this to console.log so we can check
         console.log(
@@ -141,33 +198,19 @@ document.onkeyup = function(event) {
     }
     // get win condition
     if (wordDisplayString.indexOf("_") === -1) {
-      console.log(wordDisplay);
-      // game over
-      console.log("winner winner chicken dinner");
-      // game over screen
-      alert("Winner Winner Chicken Dinner");
+      // Tick the games played and wins up
       gamesPlayed++;
       wins++;
-
-      clearGuesses();
-      setWord();
-      writeWord();
-      showWord();
-      console.log(gamesPlayed + " " + wins);
+      writeStats();
+      // display the win screen
+      winContent();
     }
   } else {
-    console.log("No more moves");
-    alert("Aw, loss");
+    // tick the games played and losses up
     gamesPlayed++;
-
-    clearGuesses();
-    setWord();
-    writeWord();
-    showWord();
-    console.log(gamesPlayed + " " + wins);
+    gamesLost++;
+    writeStats();
+    // display the lose screen
+    loseContent();
   }
 };
-// rd - at this point most of the logic has been set for this game
-
-// need to put if statements to bail out once you get to 10 guesses or you guess
-// all the letters
